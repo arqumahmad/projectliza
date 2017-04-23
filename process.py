5e2ef0ed8d068d367f1ca3a95ae281f831de0cea
kid <kid@localhost.localdomain>
2017-04-23 17:32:24 +0530

    final

--- process.py
+++ process.py
@@ -29,17 +29,23 @@ def command(data):
         else:
             local()
 
-    elif(b.find('light')>-1) and b.find('on')>-1:
+    elif(b.find('light')>-1) and ((b.find('on')>-1) or (b.find('on')>-1) or (b.find('khol')>-1) or (b.find('cool')>-1) or (b.find('holder')>-1)):
 
         GPIO.output(16,GPIO.HIGH)
-    elif(b.find('light')>-1) and(b.find('off')>-1 or b.find('of')>-1):
+    elif(b.find('light')>-1) and(b.find('off')>-1 or b.find('of')>-1 or b.find('band')>-1 or b.find('bandh')>-1 ):
         GPIO.output(16,GPIO.LOW)
 
-    elif(b.find('open')>-1) and b.find('door')>-1:
+    elif(b.find('door')>-1 or b.find('darwaza')>-1) and (b.find('on')>-1 or b.find('khol')>-1 or b.find('cool')>-1 or b.find('holder')>-1):
 
         GPIO.output(20,GPIO.HIGH)
         GPIO.output(21,GPIO.LOW)
-    elif(b.find('close')>-1) and b.find('door')>-1:
+    elif(b.find('door')>-1 or b.find('darwaza')>-1) and (b.find('close')>-1 or b.find('band')>-1 or b.find('bandh')>-1   ):
 
         GPIO.output(21,GPIO.HIGH)
         GPIO.output(20,GPIO.LOW)
+
+    elif(b.find('how')>-1) and b.find('you')>-1:
+        say("I am fine! Thank you")
+
+    elif(b.find('kesi')>-1 or b.find('kese')>-1 ) and b.find('ho')>-1:
+        sayhindi("Me achchi hu, Shukriya!")
--- tt.py
+++ tt.py
@@ -1,6 +1,26 @@
-from tts import say
-from news import world
-from news import local
-a =  local()
-b = str(a)
-say(b)
+import speech_recognition as sr
+r = sr.Recognizer()
+
+
+
+
+while True:
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+        try:
+            print "Say"
+            audio = r.listen(source, timeout=2)
+        except sr.WaitTimeoutError:
+            continue
+        except KeyboardInterrupt:
+            continue
+
+    try:
+
+        a =str(r.recognize_google(audio))
+        print a
+    except sr.UnknownValueError:
+        continue
+    except sr.RequestError as e:
+        continue
